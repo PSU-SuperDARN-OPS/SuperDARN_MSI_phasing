5f6338158c481f376d398f95837d5567a0ee3215
bgklug <bgklug@alaska.edu>
2020-03-04 17:15:40 -0900

    Remove QNX and test loops

--- phasing_calibration.c
+++ phasing_calibration.c
@@ -20,14 +20,6 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-#ifdef __QNX__
-#include <hw/pci.h>
-#include <hw/inout.h>
-#include <sys/neutrino.h>
-#include <sys/mman.h>
-#endif
-
-#include "include/registers.h"
 #include "include/phasing_cards.h"
 #include "include/utils.h"
 #include "include/pci_dio_120.h"
@@ -94,134 +86,107 @@ int main(int argc, char **argv) {
     unsigned char *BASE0, *BASE1;
     unsigned int mmap_io_ptr, IOBASE, CLOCK_RES;
     float time;
-#ifdef __QNX__
-    struct		 _clockperiod new, old;
-    struct		 timespec start_p, stop_p, start, stop, nsleep;
+    struct _clockperiod new, old;
+    struct timespec start_p, stop_p, start, stop, nsleep;
 
-    if(argc <2 ) {
-      fprintf(stderr,"%s: invoke with radar number (1 or 2 or 3)\n",argv[0]);
-      fflush(stderr);
-      exit(0);
+    if (argc < 2) {
+        fprintf(stderr, "%s: invoke with radar number (1 or 2 or 3)\n", argv[0]);
+        fflush(stderr);
+        exit(0);
     }
-    if(argc ==3 ) {
-      if(atoi(argv[2])==0) setup_flag=0;
-      else setup_flag=1;
+    if (argc == 3) {
+        if (atoi(argv[2]) == 0) setup_flag = 0;
+        else setup_flag = 1;
     } else {
-      test_flag=-1000;
+        test_flag = -1000;
     }
-    if(argc ==4 ) {
-      test_flag=atoi(argv[3]);
-      c=atoi(argv[2]);
+    if (argc == 4) {
+        test_flag = atoi(argv[3]);
+        c = atoi(argv[2]);
     } else {
-      test_flag=-1000;
+        test_flag = -1000;
     }
-    radar=atoi(argv[1]);
-    printf("Radar: %d Card: %d\n",radar,c);
-    printf("Test flag: %d\n",test_flag);
-    switch(radar) {
-      case 1:
-            portC0=PC_GRP_0;
-            portC1=PC_GRP_1;
-            portB0=PB_GRP_0;
-            portB1=PB_GRP_1;
-            portA0=PA_GRP_0;
-            portA1=PA_GRP_1;
-            cntrl0=CNTRL_GRP_0;
-            cntrl1=CNTRL_GRP_1;
-        break;
-      case 2:
-            portC0=PC_GRP_2;
-            portC1=PC_GRP_3;
-            portB0=PB_GRP_2;
-            portB1=PB_GRP_3;
-            portA0=PA_GRP_2;
-            portA1=PA_GRP_3;
-            cntrl0=CNTRL_GRP_2;
-            cntrl1=CNTRL_GRP_3;
-        break;
-      case 3:
-            portC0=PC_GRP_4;
-            portC1=PC_GRP_3;
-            portB0=PB_GRP_4;
-            portB1=PB_GRP_3;
-            portA0=PA_GRP_4;
-            portA1=PA_GRP_3;
-            cntrl0=CNTRL_GRP_4;
-            cntrl1=CNTRL_GRP_3;
-        break;
-      default:
-        fprintf(stderr,"Invalid radar number %d",radar);
-        exit(-1);
+    radar = atoi(argv[1]);
+    printf("Radar: %d Card: %d\n", radar, c);
+    printf("Test flag: %d\n", test_flag);
+    switch (radar) {
+        case 1:
+            portC0 = PC_GRP_0;
+            portC1 = PC_GRP_1;
+            portB0 = PB_GRP_0;
+            portB1 = PB_GRP_1;
+            portA0 = PA_GRP_0;
+            portA1 = PA_GRP_1;
+            cntrl0 = CNTRL_GRP_0;
+            cntrl1 = CNTRL_GRP_1;
+            break;
+        case 2:
+            portC0 = PC_GRP_2;
+            portC1 = PC_GRP_3;
+            portB0 = PB_GRP_2;
+            portB1 = PB_GRP_3;
+            portA0 = PA_GRP_2;
+            portA1 = PA_GRP_3;
+            cntrl0 = CNTRL_GRP_2;
+            cntrl1 = CNTRL_GRP_3;
+            break;
+        case 3:
+            portC0 = PC_GRP_4;
+            portC1 = PC_GRP_3;
+            portB0 = PB_GRP_4;
+            portB1 = PB_GRP_3;
+            portA0 = PA_GRP_4;
+            portA1 = PA_GRP_3;
+            cntrl0 = CNTRL_GRP_4;
+            cntrl1 = CNTRL_GRP_3;
+            break;
+        default:
+            fprintf(stderr, "Invalid radar number %d", radar);
+            exit(-1);
     }
     /* SET THE SYSTEM CLOCK RESOLUTION AND GET THE START TIME OF THIS PROCESS */
-    new.nsec=10000;
-    new.fract=0;
-    temp=ClockPeriod(CLOCK_REALTIME,&new,0,0);
-    if(temp==-1){
+    new.nsec = 10000;
+    new.fract = 0;
+    temp = ClockPeriod(CLOCK_REALTIME, &new, 0, 0);
+    if (temp == -1) {
         perror("Unable to change system clock resolution");
     }
-    temp=clock_gettime(CLOCK_REALTIME, &start_p);
-    if(temp==-1){
+    temp = clock_gettime(CLOCK_REALTIME, &start_p);
+    if (temp == -1) {
         perror("Unable to read sytem time");
     }
-    temp=ClockPeriod(CLOCK_REALTIME,0,&old,0);
-    CLOCK_RES=old.nsec;
+    temp = ClockPeriod(CLOCK_REALTIME, 0, &old, 0);
+    CLOCK_RES = old.nsec;
     printf("CLOCK_RES: %d\n", CLOCK_RES);
     /* OPEN THE PLX9656 AND GET LOCAL BASE ADDRESSES */
-    fprintf(stderr,"PLX9052 CONFIGURATION ********************\n");
+    fprintf(stderr, "PLX9052 CONFIGURATION ********************\n");
     clock_gettime(CLOCK_REALTIME, &start);
-    temp=_open_PLX9052(&pci_handle, &mmap_io_ptr, &IRQ, 1);
-    IOBASE=mmap_io_ptr;
-    if(temp==-1){
+    temp = _open_PLX9052(&pci_handle, &mmap_io_ptr, &IRQ, 1);
+    IOBASE = mmap_io_ptr;
+    if (temp == -1) {
         fprintf(stderr, "	PLX9052 configuration failed");
-    }
-    else{
+    } else {
         fprintf(stderr, "	PLX9052 configuration successful!\n");
     }
-    printf("IOBASE=%x\n",IOBASE);
+    printf("IOBASE=%x\n", IOBASE);
     /* INITIALIZE THE CARD FOR PROPER IO */
     // GROUP 0 - PortA=output, PortB=output, PortClo=output, PortChi=output
-    out8(IOBASE+cntrl0,0x80);
+    out8(IOBASE + cntrl0, 0x80);
     // GROUP 1 - PortAinput, PortB=input, PortClo=input, PortChi=output
-    out8(IOBASE+cntrl1,0x93);
-    out8(IOBASE+portA0,0x00);
-    out8(IOBASE+portB0,0x00);
-    out8(IOBASE+portC0,0x00);
-    out8(IOBASE+portA1,0x00);
-    out8(IOBASE+portB1,0x00);
-    out8(IOBASE+cntrl0,0x00);
-    out8(IOBASE+cntrl1,0x13);
-    temp=in8(IOBASE+portC1);
-    temp=temp & 0x0f;
+    out8(IOBASE + cntrl1, 0x93);
+    out8(IOBASE + portA0, 0x00);
+    out8(IOBASE + portB0, 0x00);
+    out8(IOBASE + portC0, 0x00);
+    out8(IOBASE + portA1, 0x00);
+    out8(IOBASE + portB1, 0x00);
+    out8(IOBASE + cntrl0, 0x00);
+    out8(IOBASE + cntrl1, 0x13);
+    temp = in8(IOBASE + portC1);
+    temp = temp & 0x0f;
     printf("input on group 1, port c is %x\n", temp);
-        select_card(IOBASE,c,radar);
-#endif
-    if (test_flag == -3) {
-        while (1) {
-            for (i = 0; i <= 12; i = i + 1) {
-                if (i == -1) b = 0;
-                if (i == 0) b = 1;
-                if (i > 0) b = 1 << i;
-                b = b | 0x200;
-                printf("Selecting Beamcode: %d 0x%x\n", b, b);
-                beam_code(IOBASE, b);
-                sleep(1);
-            }
-        }
-    }
-    if (test_flag >= 0) {
-        beam_code(IOBASE, test_flag);
-        temp = verify_data(IOBASE, c, test_flag, test_flag);
-        exit(0);
-    }
-    if (test_flag == -2) {
-        printf("test flag %d radar %d icard %d verify programming\n", test_flag, radar, c);
-        for (b = 0; b <= 8191; b++) {
-            usleep(10000);
-            temp = verify_data(IOBASE, c, b, b);
-        }
-        exit(0);
-    }
+    select_card(IOBASE, c, radar);
+
+
     fnum = atoi(freq_steps);
     fstart = atof(freq_start);
     fstop = atof(freq_stop);
@@ -320,19 +285,7 @@ int main(int argc, char **argv) {
             }
             usleep(10000);
         } //test flag if
-        if (stupid_flag) {
-            while (1) {  //JDS
-                if (b == 0) b = 8; //JDS
-                else b = 0;//JDS
-                data = b;
-                beamcode = b;
 
-                printf("B: %d data: %d BC: %d\n", b, data, beamcode);
-                temp = write_data(IOBASE, c, beamcode, data);
-                sleep(2); //JDS
-                temp = write_attenuators(IOBASE, c, beamcode, 0);
-            }
-        }
 
         printf("Programming all zeros attenuation coding\n");
         for (b = 0; b < ATTENCODES; b++) {
@@ -345,11 +298,9 @@ int main(int argc, char **argv) {
 
         printf("Verifying all zero programming attenuation coding\n");
         for (b = 0; b < ATTENCODES; b++) {
-            select_card(IOBASE, c);
-            beam_code(IOBASE, b);
-            usleep(10000);
             temp = verify_attenuators(IOBASE, c, b, 0);
         }
+
         printf("Programming 1-to-1 attenuation coding no phase\n");
         for (b = 0; b < ATTENCODES; b++) {
             data = b;
@@ -358,11 +309,9 @@ int main(int argc, char **argv) {
             temp = write_data(IOBASE, c, beamcode, 0);
             temp = write_attenuators(IOBASE, c, beamcode, b);
         }
+
         printf("Verifying 1-to-1 programming attenuation coding\n");
         for (b = 0; b < ATTENCODES; b++) {
-            select_card(IOBASE, c);
-            beam_code(IOBASE, b);
-            usleep(10000);
             temp = verify_attenuators(IOBASE, c, b, b);
         }
 
@@ -377,9 +326,6 @@ int main(int argc, char **argv) {
         }
         printf("Verifying all ones programming phase coding\n");
         for (b = 0; b < PHASECODES; b++) {
-            select_card(IOBASE, c);
-            beam_code(IOBASE, b);
-            usleep(10000);
             temp = verify_data(IOBASE, c, b, 8191);
         }
 
@@ -395,15 +341,10 @@ int main(int argc, char **argv) {
         }
         printf("Verifying 1-to-1 programming phase coding\n");
         for (b = 0; b < PHASECODES; b++) {
-            select_card(IOBASE, c);
-            beam_code(IOBASE, b);
-            usleep(10000);
             temp = verify_data(IOBASE, c, b, b);
         }
 
-        if (test_flag == -1) {
-            exit(0);
-        }
+
         sleep(10);
         if (verbose > 0) fprintf(stdout, "Measuring phases\n");
 #ifdef _QUICK_
@@ -456,11 +397,9 @@ int main(int argc, char **argv) {
             gettimeofday(&t10, NULL);
             button_command(sock, ":INIT1:IMM\r\n", 0, verbose);
             if (b == 0) sleep(1);
-#ifdef __QNX__
-            usleep(wait_delay_ms*1000); //NO AVERAGE
-#else
+
             usleep(wait_delay_ms * 1000);
-#endif
+
             take_data = 1;
             attempt = 0;
             while ((take_data) && (attempt < max_attempts)) {
@@ -510,4 +449,5 @@ int main(int argc, char **argv) {
         c = -1;
     } // end of Card loop
 }
+
 
