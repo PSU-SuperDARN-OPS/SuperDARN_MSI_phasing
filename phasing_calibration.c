5e43305a85463e52ce39244d40e24b5cdeadfb6e
bgklug <bgklug@alaska.edu>
2020-03-18 11:34:15 -0800

    Add command line interface

--- phasing_calibration.c
+++ phasing_calibration.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <ctype.h>
 
 #include "include/phasing_cards.h"
 #include "include/utils.h"
@@ -35,29 +36,77 @@ int main(int argc, char **argv) {
     double freq[VNA_FREQS];
     double pd_old, pd_new, phase_diff = 0.0;
     int32_t count;
-    int32_t i = 0, c = 31, data = 0, wait_delay_ms = 30;
+    int32_t i = 0, card = 31, data = 0, wait_delay_ms = 30;
     int b = 0;
     int last_collect, current_collect, collect = 0, beamcode = 0, take_data = 0, attempt = 0, max_attempts = 20;
     double fstart;
     double fstop;
     double fstep;
     int fnum;
-    uint_fast8_t radar;
+    uint_fast8_t radar_number;
     char serial_number[80] = "";
     int temp;
 
-    struct DIO phasing_matrix;
+    int rval, nflag = 0, iflag = 0, cflag = 0, rflag = 0, sflag = 0;
 
-    if (argc < 2) {
-        fprintf(stderr, "%s: invoke with radar number (1 or 2 or 3)\n", argv[0]);
-        fflush(stderr);
-        exit(0);
+    struct DIO phasing_matrix;
+/***************************************************************
+     * COMMAND LINE ARGUMENTS
+     **************************************************************/
+    while ((rval = getopt(argc, argv, "+r:n:c:p:v:s:ih")) != -1) {
+        switch (rval) {
+            case 'v':
+                verbose = atoi(optarg);
+                break;
+            case 'n':
+                radar_number = atoi(optarg);
+                nflag = 1;
+                break;
+            case 'c':
+                card = atoi(optarg);
+                cflag = 1;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'r':
+                snprintf(radar_name, 16, "%s", optarg);
+                rflag = 1;
+                break;
+            case 'i':
+                iflag = 1;
+                break;
+            case 's':
+                snprintf(serial_number, 16, "%s", optarg);
+                sflag = 1;
+                break;
+            case '?':
+                if (optopt == 'r' || optopt == 'c' || optopt == 'n' || optopt == 'p' || optopt == 's' ||
+                    optopt == 'a' || optopt == 'v')
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf(stderr,
+                            "Unknown option character `\\x%x'.\n",
+                            optopt);
+                return 1;
+            case 'h':
+            default:
+                fprintf(stderr,
+                        "Required:\n  -r radarname\n  -n dio radar number (1 or 2)\n  -c card number\n  -s serial number\nOptional:\n  -a vna ipaddress\n  -p vna tcp port\n  -i to run vna init and cal process\n  -v number to set verbose output level\n");
+                return 1;
+        }
+    }
+    if (argc == 1 || radar_number == 0 || rflag == 0 || nflag == 0 || cflag == 0 || sflag == 0) {
+        fprintf(stderr,
+                "Required arguments -r radarname, -n dio radar number, -s serial number, and -c card number\n Consult the help using the -h option\n");
+        return 1;
     }
 
-    radar = atoi(argv[1]);
-    printf("Radar: %d Card: %d\n", radar, c);
+    printf("Radar: %d Card: %d\n", radar_number, card);
 
-    phasing_matrix.radar_number = radar;
+    phasing_matrix.radar_number = radar_number;
     temp = set_ports(&phasing_matrix);
 
     if (temp == -1) {
@@ -69,7 +118,7 @@ int main(int argc, char **argv) {
 
     /* INITIALIZE THE CARD FOR PROPER IO */
     init_phasing_cards(&phasing_matrix);
-    select_card(&phasing_matrix, c);
+    select_card(&phasing_matrix, card);
 
     fnum = atoi(freq_steps);
     fstart = atof(freq_start);
@@ -86,32 +135,24 @@ int main(int argc, char **argv) {
     if (verbose > 0) printf("Opening Socket %s %d\n", hostip, port);
     init_vna(hostip, port);
 
-    calibrate_vna(freq_start, freq_stop, freq_steps);
+    if (iflag) {
+        calibrate_vna(freq_start, freq_stop, freq_steps);
 
-    vna_trigger_single(verbose);
-    printf("\n\nCalibration Complete\nReconfigure for Phasing Card Measurements\n");
-    mypause();
+        vna_trigger_single(verbose);
+        printf("\n\nCalibration Complete\nReconfigure for Phasing Card Measurements\n");
+        mypause();
+    }
 
-    c = -1;
-    printf("\n\nEnter Radar Name: ");
-    fflush(stdin);
-    scanf("%s", radar_name);
-    fflush(stdout);
-    fflush(stdin);
-    printf("\n\nEnter Phasing Card Number: ");
-    fflush(stdin);
-    fflush(stdout);
-    scanf("%d", &c);
     printf("\n\nEnter Serial Number: ");
     fflush(stdin);
     fflush(stdout);
     scanf("%s", serial_number);
-    printf("Radar: <%s>  Card: %d Serial: %s\n", radar_name, c, serial_number);
+    printf("Radar: <%s>  Card: %d Serial: %s\n", radar_name, card, serial_number);
     fflush(stdout);
 
 
 
-    sprintf(filename, "%s%s_%s_%02d_%s%s", dir, file_prefix, radar_name, c, serial_number, file_ext);
+    sprintf(filename, "%s%s_%s_%02d_%s%s", dir, file_prefix, radar_name, card, serial_number, file_ext);
     if (verbose > 0) fprintf(stdout, "Using file: %s\n", filename);
     fflush(stdout);
 
@@ -142,13 +183,13 @@ int main(int argc, char **argv) {
         data = 0;
         beamcode = b;
 
-        temp = write_data(&phasing_matrix, c, beamcode, data);
-        temp = write_attenuators(&phasing_matrix, c, beamcode, data);
+        temp = write_data(&phasing_matrix, card, beamcode, data);
+        temp = write_attenuators(&phasing_matrix, card, beamcode, data);
     }
 
     printf("Verifying all zero programming attenuation coding\n");
     for (b = 0; b < ATTENCODES; b++) {
-        temp = verify_attenuators(&phasing_matrix, c, b, 0);
+        temp = verify_attenuators(&phasing_matrix, card, b, 0);
     }
 
     printf("Programming 1-to-1 attenuation coding no phase\n");
@@ -156,13 +197,13 @@ int main(int argc, char **argv) {
         data = b;
         beamcode = b;
 
-        temp = write_data(&phasing_matrix, c, beamcode, 0);
-        temp = write_attenuators(&phasing_matrix, c, beamcode, b);
+        temp = write_data(&phasing_matrix, card, beamcode, 0);
+        temp = write_attenuators(&phasing_matrix, card, beamcode, b);
     }
 
     printf("Verifying 1-to-1 programming attenuation coding\n");
     for (b = 0; b < ATTENCODES; b++) {
-        temp = verify_attenuators(&phasing_matrix, c, b, b);
+        temp = verify_attenuators(&phasing_matrix, card, b, b);
     }
 
     printf("Programming all ones phase coding\n");
@@ -171,12 +212,12 @@ int main(int argc, char **argv) {
         beamcode = b;
 
         if (b % 512 == 0) printf("B: %d data: %d BC: %d\n", b, data, beamcode);
-        temp = write_data(&phasing_matrix, c, beamcode, 8191);
-        temp = write_attenuators(&phasing_matrix, c, beamcode, 63);
+        temp = write_data(&phasing_matrix, card, beamcode, 8191);
+        temp = write_attenuators(&phasing_matrix, card, beamcode, 63);
     }
     printf("Verifying all ones programming phase coding\n");
     for (b = 0; b < PHASECODES; b++) {
-        temp = verify_data(&phasing_matrix, c, b, 8191);
+        temp = verify_data(&phasing_matrix, card, b, 8191);
     }
 
     printf("Programming 1-to-1 phase coding no attenuation\n");
@@ -185,13 +226,13 @@ int main(int argc, char **argv) {
         beamcode = b;
 
         if (b % 512 == 0) printf("B: %d data: %d BC: %d\n", b, data, beamcode);
-        temp = write_data(&phasing_matrix, c, beamcode, 0);
-        temp = write_data(&phasing_matrix, c, beamcode, data);
-        temp = write_attenuators(&phasing_matrix, c, beamcode, 0);
+        temp = write_data(&phasing_matrix, card, beamcode, 0);
+        temp = write_data(&phasing_matrix, card, beamcode, data);
+        temp = write_attenuators(&phasing_matrix, card, beamcode, 0);
     }
     printf("Verifying 1-to-1 programming phase coding\n");
     for (b = 0; b < PHASECODES; b++) {
-        temp = verify_data(&phasing_matrix, c, b, b);
+        temp = verify_data(&phasing_matrix, card, b, b);
     }
 
 
@@ -202,7 +243,7 @@ int main(int argc, char **argv) {
     current_collect = 0;
     for (b = 0; b < PHASECODES; b++) {
         beamcode = b;
-        temp = select_card(&phasing_matrix, c);
+        temp = select_card(&phasing_matrix, card);
         select_beam_code(&phasing_matrix, beamcode);
         collect = 0;
         if (b == 0) collect = 1;
@@ -212,26 +253,26 @@ int main(int argc, char **argv) {
         if (b == 4) collect = 1;
         if ((b % 8) == 0) collect = 1;
         if (!collect) {
-            printf(":::Card %d::: Skipping BEAMCode %d :: %d\n", c, beamcode, b % 8);
+            printf(":::Card %d::: Skipping BEAMCode %d :: %d\n", card, beamcode, b % 8);
             continue;
         } else {
-            printf(":::Card %d::: Reading BEAMCode %d :: %d\n", c, beamcode, b % 8);
+            printf(":::Card %d::: Reading BEAMCode %d :: %d\n", card, beamcode, b % 8);
             last_collect = current_collect;
             current_collect = b;
         }
 
-        temp = verify_data(&phasing_matrix, c, b, b);
+        temp = verify_data(&phasing_matrix, card, b, b);
 
         if (temp != 0) {
             data = b;
 
             if (b % 512 == 0) printf("B: %d data: %d BC: %d\n", b, data, beamcode);
-            temp = write_data(&phasing_matrix, c, beamcode, 0);
-            temp = write_data(&phasing_matrix, c, beamcode, data);
-            temp = write_attenuators(&phasing_matrix, c, beamcode, 0);
+            temp = write_data(&phasing_matrix, card, beamcode, 0);
+            temp = write_data(&phasing_matrix, card, beamcode, data);
+            temp = write_attenuators(&phasing_matrix, card, beamcode, 0);
 
             usleep(10000);
-            temp = verify_data(&phasing_matrix, c, b, b);
+            temp = verify_data(&phasing_matrix, card, b, b);
         }
         if (temp != 0) {
             printf("Failed Verification for beamcode: %d  %x\n", b, b);
@@ -283,11 +324,10 @@ int main(int argc, char **argv) {
         fwrite(&i, sizeof(int32_t), 1, calfile);
         count = fwrite(phase[i], sizeof(double), PHASECODES, calfile);
         count = fwrite(pwr_mag[i], sizeof(double), PHASECODES, calfile);
-        //printf("Freq index: %d Count: %d\n",i,count);
     }
     printf("Closing File\n");
     fclose(calfile);
-    c = -1;
+    card = -1;
 }
 
 
